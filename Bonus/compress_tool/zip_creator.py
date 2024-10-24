c872f6ee8fb89687779f451cff46b06eb37d03fa
Julio Tencio <j.tenciocoto@gmail.com>
2024-10-24 10:58:30 -0600

    fixing filepath

--- Bonus/compress_tool/zip_creator.py
+++ Bonus/compress_tool/zip_creator.py
@@ -5,7 +5,8 @@ def make_archive(filepaths, dest_dir):
     dest_path = pathlib.Path(dest_dir, "compressed.zip")
     with zipfile.ZipFile(dest_path, 'w') as archive:
         for filepath in filepaths:
-            archive.write(filepath)
+            filepath = pathlib.Path(filepath)
+            archive.write(filepath, acrname=filepath.name)
             
 if __name__ == "__main__":
     make_archive(filepaths=["test1",  "test2", "test3"], dest_dir="dest")
